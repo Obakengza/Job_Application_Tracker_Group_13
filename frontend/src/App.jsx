2b76dedf9e90aadeba2a272921bff084e34809e5
Obakeng ZA <obakeng@gmail.com>
2026-05-12 20:24:40 +0200

    saving my work before pull

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,12 @@ import ProfilePage from "./components/ProfilePage";
 import JobTrackingPage from "./pages/JobTrackingPage";
 import JobPostsPage from "./pages/JobPostsPage";
 import LoginPage from "./pages/LoginPage";
-<<<<<<< HEAD
 import AdminLoginPage from "./pages/admin/AdminLoginPage";
 import AdminJobPosts from "./pages/admin/AdminJobPosts";
-=======
-import JobPostsPage from "./pages/JobPostsPage";
->>>>>>> eee04d7406b7cd3abfa778c1253c0fdaf19449a0
 
 function App() {
   const location = useLocation();
 
-  // Hide sidebar on login and admin pages
   const hideSidebar =
     location.pathname === "/login" || location.pathname.startsWith("/admin");
 
@@ -28,13 +23,9 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/" element={<ProfilePage />} />
           <Route path="/tracking" element={<JobTrackingPage />} />
-<<<<<<< HEAD
           <Route path="/jobs" element={<JobPostsPage />} />
           <Route path="/admin" element={<AdminLoginPage />} />
           <Route path="/admin/dashboard" element={<AdminJobPosts />} />
-=======
-          <Route path="/posts" element={<JobPostsPage />} />
->>>>>>> eee04d7406b7cd3abfa778c1253c0fdaf19449a0
         </Routes>
       </div>
     </div>
