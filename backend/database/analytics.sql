a6af941716b0c5da3d78766b54b625140328189b
Motlalepula-Tshabalala <motlalepulafana16@gmail.com>
2026-05-16 20:11:39 +0200

    Updated Phase 3 database implementation using raw SQL

--- backend/database/analytics.sql
+++ backend/database/analytics.sql
@@ -1,19 +1,3 @@
--- Total applications
-SELECT COUNT(*) AS total_applications
-FROM job_application;
-
-
--- Applications per status
-SELECT
-    s.status_name,
-    COUNT(*) AS total
-FROM job_application ja
-JOIN application_status s
-ON ja.status_id = s.status_id
-GROUP BY s.status_name;
-
-
--- Companies receiving most applications
 SELECT
     c.company_name,
     COUNT(*) AS applications
@@ -23,4 +7,4 @@ ON c.company_id = jp.company_id
 JOIN job_application ja
 ON jp.job_post_id = ja.job_post_id
 GROUP BY c.company_name
-ORDER BY applications DESC;
+ORDER BY applications DESC; 
